dc3727a6718ddb770a4a0935c45106217ddbfa98
Nguyễn Văn Quân <37933762+vanquan223@users.noreply.github.com>
2018-04-10 13:21:04 +0700

    Initial commit

---
+++ app/src/main/java/vn/vnpt/vanquan223/bai1/MainActivity.java
@@ -0,0 +1,36 @@
+package vn.vnpt.vanquan223.bai1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.id_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.bai1);
+                ImageButton imageButton = (ImageButton) findViewById(R.id.image_button);
+                ImageView imageView = (ImageView) findViewById(R.id.image_view);
+            }
+        });
+
+
+        /*imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setVisibility(View.VISIBLE);
+            }
+        });*/
+    }
+}
